4bbf5b7ec8206e456b20c4377f8a661f785ccae9
ikust <ikustov.dev@gmail.com>
2024-09-08 21:19:51 +0200

    styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-
+import styles from './home.module.scss';
 import WebApp from '@twa-dev/sdk'
 import { useEffect, useState } from 'react'
 
@@ -23,22 +23,24 @@ export default function Home() {
   }, [])
 
   return (
-    <main className="p-4">
-      {userData ? (
-        <>
-          <h1 className="text-2xl font-bold mb-4">User Data</h1>
-          <ul>
-            <li>ID: {userData.id}</li>
-            <li>First Name: {userData.first_name}</li>
-            <li>Last Name: {userData.last_name || 'N/A'}</li>
-            <li>Username: {userData.username || 'N/A'}</li>
-            <li>Language Code: {userData.language_code}</li>
-            <li>Is Premium: {userData.is_premium ? 'Yes' : 'No'}</li>
-          </ul>
-        </>
-      ) : (
-        <div>Loading...</div>
-      )}
-    </main>
+    <div className={styles.myComponent}>
+      <main className="p-4">
+        {userData ? (
+          <>
+            <h1 className="text-2xl font-bold mb-4">User Data</h1>
+            <ul>
+              <li>ID: {userData.id}</li>
+              <li>First Name: {userData.first_name}</li>
+              <li>Last Name: {userData.last_name || 'N/A'}</li>
+              <li>Username: {userData.username || 'N/A'}</li>
+              <li>Language Code: {userData.language_code}</li>
+              <li>Is Premium: {userData.is_premium ? 'Yes' : 'No'}</li>
+            </ul>
+          </>
+        ) : (
+          <div>Loading...</div>
+        )}
+      </main>
+    </div>
   )
 }
